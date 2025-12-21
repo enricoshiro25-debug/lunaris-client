8d2c2b555fbb0d14c7db12eb6d64b9e487487124
enricoshiro25-debug <enricoshiro25@gmail.com>
2025-12-21 19:40:57 +0100

    Update dashboard.js

--- public/dashboard.js
+++ public/dashboard.js
@@ -6,7 +6,48 @@ if (!username) {
 
 document.getElementById("user").textContent = username;
 
+// LISTE AVATAR
+let faceIndex = 0;
+let robeIndex = 0;
+
+const faces = ["face1.png", "face2.png", "face3.png"];
+const robes = ["robe1.png", "robe2.png", "robe3.png"];
+
+function updateAvatar() {
+  document.getElementById("face").src =
+    "/images/avatars/face/" + faces[faceIndex];
+
+  document.getElementById("robe").src =
+    "/images/avatars/robe/" + robes[robeIndex];
+}
+
+// CONTROLLI TESTA
+function nextFace() {
+  faceIndex = (faceIndex + 1) % faces.length;
+  updateAvatar();
+}
+
+function prevFace() {
+  faceIndex = (faceIndex - 1 + faces.length) % faces.length;
+  updateAvatar();
+}
+
+// CONTROLLI VESTITO
+function nextRobe() {
+  robeIndex = (robeIndex + 1) % robes.length;
+  updateAvatar();
+}
+
+function prevRobe() {
+  robeIndex = (robeIndex - 1 + robes.length) % robes.length;
+  updateAvatar();
+}
+
+// LOGOUT
 document.getElementById("logout").addEventListener("click", () => {
   localStorage.removeItem("username");
   window.location.href = "index.html";
 });
+
+// INIT
+updateAvatar();
