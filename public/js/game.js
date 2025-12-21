30583ac8670d371a7cb60054ba15ce818f1562c2
enricoshiro25-debug <enricoshiro25@gmail.com>
2025-12-21 02:05:16 +0100

    Update game.js

--- public/js/game.js
+++ public/js/game.js
@@ -1,1 +1,21 @@
+const socket = io();
+
+// invio posizione
+function sendMove(x,y) {
+  socket.emit("move",{x,y});
+}
+
+// ricezione aggiornamento
+socket.on("updatePosition", data=>{
+  // per ora log
+  console.log("Player si muove a:", data);
+});
+
+// chat
+function sendChat(msg){
+  socket.emit("chat", msg);
+}
+socket.on("chatMessage", msg=>{
+  console.log("Chat:", msg);
+});
 
