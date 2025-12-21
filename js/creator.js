33601ea09b86552199f6eea8031e737504c2d0d7
enricoshiro25-debug <enricoshiro25@gmail.com>
2025-12-21 13:23:27 +0100

    Delete js directory

--- js/creator.js
+++
@@ -1,9 +0,0 @@
-function setHair(n) {
-  document.getElementById("hair").src =
-    `images/avatar/hair/hair${n}.png`;
-}
-
-function setOutfit(n) {
-  document.getElementById("outfit").src =
-    `images/avatar/outfits/robe${n}.png`;
-}
