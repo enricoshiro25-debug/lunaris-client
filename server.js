91230a8d1525de2fd713d25bccaf82840eb6ca85
enricoshiro25-debug <enricoshiro25@gmail.com>
2025-12-21 14:33:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server avviato');
+  console.log("Server avviato");
 });
