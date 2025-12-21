d4f4f76a3a09ec6d83cbdb39a943b1e3970cb04e
enricoshiro25-debug <enricoshiro25@gmail.com>
2025-12-21 01:28:46 +0100

    Update creator.js

--- js/creator.js
+++ js/creator.js
@@ -1,1 +1,26 @@
+function setBody(file) {
+  document.getElementById("body").src =
+    "images/avatar/body/" + file;
+}
+
+function setHair(file) {
+  document.getElementById("hair").src =
+    "images/avatar/hair/" + file;
+}
+
+function setOutfit(file) {
+  document.getElementById("outfit").src =
+    "images/avatar/outfits/" + file;
+}
+
+function savePG() {
+  const pg = {
+    body: body.src,
+    hair: hair.src,
+    outfit: outfit.src
+  };
+
+  localStorage.setItem("lunaris_pg", JSON.stringify(pg));
+  msg.innerText = "Personaggio salvato ✨";
+}
 
