0efdf917d0559d88b391df5c9a4c43aa85f03eb3
enricoshiro25-debug <enricoshiro25@gmail.com>
2025-12-21 02:36:04 +0100

    Update avatar.js

--- public/js/avatar.js
+++ public/js/avatar.js
@@ -1,32 +1,37 @@
-const bodySelect = document.getElementById("bodySelect");
-const hairSelect = document.getElementById("hairSelect");
-const faceSelect = document.getElementById("faceSelect");
+window.onload = function(){
 
-const avatarBody = document.getElementById("avatarBody");
-const avatarHair = document.getElementById("avatarHair");
-const avatarFace = document.getElementById("avatarFace");
+  // Variabile globale temporanea per l'utente (da sostituire con login)
+  const username = "prova";  // Cambia con username reale
 
-// Aggiorna anteprima
-bodySelect.addEventListener("change", ()=>{ avatarBody.src = `images/avatars/body/${bodySelect.value}.png`; });
-hairSelect.addEventListener("change", ()=>{ avatarHair.src = `images/avatars/hair/${hairSelect.value}.png`; });
-faceSelect.addEventListener("change", ()=>{ avatarFace.src = `images/avatars/face/${faceSelect.value}.png`; });
+  const bodySelect = document.getElementById("bodySelect");
+  const hairSelect = document.getElementById("hairSelect");
+  const faceSelect = document.getElementById("faceSelect");
 
-// Salva avatar
-function saveAvatar(){
-  const avatarData = {
-    body: bodySelect.value,
-    hair: hairSelect.value,
-    face: faceSelect.value
-  };
+  const avatarBody = document.getElementById("avatarBody");
+  const avatarHair = document.getElementById("avatarHair");
+  const avatarFace = document.getElementById("avatarFace");
 
-  const username = localStorage.getItem("lunaris_user"); // memorizzato al login
+  // Aggiorna anteprima al cambio selezione
+  bodySelect.addEventListener("change", ()=>{ avatarBody.src = `images/avatars/body/${bodySelect.value}.png`; });
+  hairSelect.addEventListener("change", ()=>{ avatarHair.src = `images/avatars/hair/${hairSelect.value}.png`; });
+  faceSelect.addEventListener("change", ()=>{ avatarFace.src = `images/avatars/face/${faceSelect.value}.png`; });
 
-  fetch("/saveAvatar",{
-    method:"POST",
-    headers:{"Content-Type":"application/json"},
-    body: JSON.stringify({username, avatar: avatarData})
-  })
-  .then(res=>res.text())
-  .then(msg=>alert("Avatar salvato!"))
-  .catch(err=>alert("Errore: "+err));
-}
+  // Salva avatar sul server
+  document.getElementById("saveAvatarBtn").addEventListener("click", ()=>{
+    const avatarData = {
+      body: bodySelect.value,
+      hair: hairSelect.value,
+      face: faceSelect.value
+    };
+
+    fetch("/saveAvatar",{
+      method:"POST",
+      headers:{"Content-Type":"application/json"},
+      body: JSON.stringify({username, avatar: avatarData})
+    })
+    .then(res=>res.text())
+    .then(msg=>alert("Avatar salvato!"))
+    .catch(err=>alert("Errore: "+err));
+  });
+
+};
